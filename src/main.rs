6cbf92fb476f732cb6c4e74f38549143a718b7fc
Shivan Vickers <shivan.vickers@gmail.com>
2025-10-16 00:02:58 -0700

    chapter 2 day 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
+#![warn(clippy::all, clippy::pedantic)]
+
+use std::io::stdin;
+
+struct Visitor {
+    name: String,
+    greeting: String,
+}
+
+impl Visitor {
+    fn new(name: &str, greeting: &str) -> Self {
+        Self {
+            name: name.to_lowercase(),
+            greeting: greeting.to_string(),
+        }
+    }
+
+    fn greet_visitor(&self) {
+        println!("{}", self.greeting);
+    }
+}
+
+fn what_is_you_name() -> String {
+    let mut your_name: String = String::new();
+
+    stdin()
+        .read_line(&mut your_name)
+        .expect("Failed to read line");
+
+    your_name.trim().to_lowercase()
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, what's your name?");
+
+    let name: String = what_is_you_name();
+
+    let visitor_list = [
+        Visitor::new("bert", "Hello Bert, enjoy your treehouse."),
+        Visitor::new("steve", "Hi Steve. Your milk is in the fridge."),
+        Visitor::new("fred", "Wow, who invited Fred?"),
+    ];
+
+    let known_visitor = visitor_list.iter().find(|visitor| visitor.name == name);
+
+    match known_visitor {
+        Some(visitor) => visitor.greet_visitor(),
+        None => println!("You are not on the visitor list. Please leave."),
+    }
 }
